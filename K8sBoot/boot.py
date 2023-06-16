0727b9b78af986701b6afa848172219435f94382
shijianhang <3076679995@qq.com>
2023-06-16 16:43:25 +0800

    完善configmap/secret挂载+探针

--- K8sBoot/boot.py
+++ K8sBoot/boot.py
@@ -5,12 +5,12 @@ import fnmatch
 import hashlib
 import json
 import os
+from urllib import parse
 from pyutilb.util import *
 from pyutilb.file import *
 from pyutilb.cmd import *
 from pyutilb import YamlBoot, BreakException
 from pyutilb.log import log
-import platform
 
 '''
 k8s配置生成的基于yaml的启动器
@@ -29,13 +29,14 @@ class Boot(YamlBoot):
             'ns': self.ns,
             'app': self.app,
             'config': self.config,
+            'config_files': self.config_files,
             'secret': self.secret,
+            'secret_files': self.secret_files,
             'rc': self.rc,
             'rs': self.rs,
             'ds': self.ds,
             'sts': self.sts,
             'deploy': self.deploy,
-            'service': self.service,
             'containers': self.containers,
         }
         self.add_actions(actions)
@@ -49,6 +50,10 @@ class Boot(YamlBoot):
 
         self._ns = '' # 命名空间
         self._app = '' # 应用名
+        self._config_data = {} # 记录设置过的配置
+        self._config_file_keys = [] # 记录文件类型的key
+        self._secret_data = {} # 记录设置过的密文
+        self._secret_file_keys = [] # 记录文件类型的key
         self._containers = [] # 记录处理过的容器
         self._volumes = [] # 记录容器中的卷
         self._ports = [] # 记录容器中的端口映射
@@ -57,6 +62,10 @@ class Boot(YamlBoot):
     # 清空app相关的属性
     def clear_app(self):
         self._app = ''  # 应用名
+        self._config_data = {}  # 记录设置过的配置
+        self._config_file_keys = [] # 记录文件类型的key
+        self._secret_data = {}  # 记录设置过的密文
+        self._secret_file_keys = [] # 记录文件类型的key
         self._containers = []  # 记录处理过的容器
         self._volumes = []  # 记录容器中的卷
         self._ports = []  # 记录容器中的端口映射
@@ -127,47 +136,124 @@ class Boot(YamlBoot):
         self._app = name
         # 执行子步骤
         self.run_steps(steps)
-        # 暴露端口
+        # 生成configmap
+        self.configmap()
+        # 生成secret
+        self.secretmap()
+        # 生成service：暴露端口
         self.service()
         # 清空app相关的属性
         self.clear_app()
 
-    def config(self, items):
+    def config(self, data):
         '''
-        生成配置
-        :param items 配置项，可以是键值对，可以包含变量，如
+        以键值对的方式来设置配置
+        :param data 配置项，可以是键值对，可以包含变量，如
               name: shigebeyond
               nginx.conf: ${read_file(./nginx.conf)}
               也可以是变量表达式，如 $cfg 或 ${read_yaml(./cfg.yml)}
         '''
         # 替换变量，支持items是str或dict
-        items = replace_var(items, False)
-        yaml = {
-            "apiVersion": "v1",
-            "kind": "ConfigMap",
-            "metadata": self.build_metadata("-cfg"),
-            "data": items
-        }
-        self.save_yaml(yaml, '-config.yml')
+        data = replace_var(data, False)
+        if not isinstance(data, dict):
+            raise Exception('config动作参数只接受dict类型')
+        self._config_data.update(data)
 
-    def secret(self, items):
+    def config_files(self, files):
+        '''
+        以文件内容的方式来设置配置，在挂载configmap时items默认填充用config_files()写入的key
+        :param files 配置文件list或dict或目录
+                  dict类型： key是配置项名，value是文件路径，如 nginx.conf: ./nginx.conf
+                  list类型： 元素是文件路径，会用文件名作为key
+                  str类型： 目录/文件路径
+        '''
+        files = self.build_config_data_from_files(files)
+        self._config_file_keys.extend(files.keys())
+        self._config_data.update(files)
+
+    def build_config_data_from_files(self, files):
+        '''
+        以读文件内容的方式来构建配置数据
+        :param files 配置文件list或dict或目录
+                  dict类型： key是配置项名，value是文件路径，如 nginx.conf: ./nginx.conf
+                  list类型： 元素是文件路径，会用文件名作为key
+                  str类型： 目录/文件路径
+        '''
+        # 1 dict
+        if isinstance(files, dict):
+            for key, file in files.items():
+                files[key] = read_file(file)
+            return files
+
+        # 2 str: 目录/文件转list
+        if isinstance(files, str):
+            path = files
+            if not os.path.exists(path):
+                raise Exception(f"config_files/secret_files动作参数[{path}]因是str类型而被认定为目录或文件，但目录或文件不存在")
+            if os.path.isdir(path): # 目录
+                files = [os.path.join(path, f) for f in os.listdir(path)]
+            else: # 文件
+                files = [path]
+
+        # 3 list
+        if isinstance(files, list):
+            ret = {}
+            for file in files:
+                key = os.path.basename(file) # 文件名作为key
+                ret[key] = read_file(file)
+            return ret
+
+        # 4 其他: 报错
+        raise Exception(f"config_files/secret_files动作参数只接受dict/list/str类型，而实际参数是: {files}")
+
+    # 生成配置
+    def configmap(self):
+        if self._config_data:
+            yaml = {
+                "apiVersion": "v1",
+                "kind": "ConfigMap",
+                "metadata": self.build_metadata("-cfg"),
+                "data": self._config_data
+            }
+            self.save_yaml(yaml, '-config.yml')
+
+    def secret(self, data):
         '''
         生成密钥， 其实跟config差不多，只不过config是明文，secret是密文
-        :param items 密钥项，可以是键值对，可以包含变量，如
+        :param data 密钥项，可以是键值对，可以包含变量，如
               name: shigebeyond
               nginx.conf: ${read_file(./nginx.conf)}
               也可以是变量表达式，如 $cfg 或 ${read_yaml(./cfg.yml)}
         '''
         # 替换变量，支持items是str或dict
-        items = replace_var(items, False)
-        yaml = {
-            "apiVersion": "v1",
-            "kind": "Secret",
-            "metadata": self.build_metadata('-secret'),
-            "type": "Opaque",
-            "data": items
-        }
-        self.save_yaml(yaml, '-secret.yml')
+        data = replace_var(data, False)
+        if not isinstance(data, dict):
+            raise Exception('secret动作参数只接受dict类型')
+        self._secret_data.update(data)
+
+    def secret_files(self, files):
+        '''
+        以文件内容的方式来设置配置，在挂载secret时items默认填充用secret_files()写入的key
+        :param files 配置文件list或dict或目录
+                  dict类型： key是配置项名，value是文件路径，如 nginx.conf: ./nginx.conf
+                  list类型： 元素是文件路径，会用文件名作为key
+                  str类型： 目录/文件路径
+        '''
+        files = self.build_config_data_from_files(files)
+        self._secret_file_keys.extend(files.keys())
+        self._secret_data.update(files)
+
+    # 生成secret
+    def secretmap(self):
+        if self._secret_data:
+            yaml = {
+                "apiVersion": "v1",
+                "kind": "Secret",
+                "metadata": self.build_metadata('-secret'),
+                "type": "Opaque",
+                "data": self._secret_data
+            }
+            self.save_yaml(yaml, '-secret.yml')
 
     # 修正有副本的选项
     def fix_replicas_option(self, option, action):
@@ -227,7 +313,7 @@ class Boot(YamlBoot):
         '''
         生成ds
         :params option 部署选项 {node}
-                        node 节点过滤，如 "beta.kubernetes.io/os": "linux" 或 "disk": "ssd"
+                        node 节点选择，如 "beta.kubernetes.io/os": "linux" 或 "disk": "ssd"
         '''
         option = self.fix_replicas_option(option, 'ds')
         yaml = {
@@ -238,7 +324,7 @@ class Boot(YamlBoot):
                 "selector": {
                     "matchLabels": self.build_labels()
                 },
-                "template": self.build_pod_template(option.get('node'), option.get('tolerations'))
+                "template": self.build_pod_template(option.get('nodes'), option.get('tolerations'))
             }
         }
 
@@ -249,7 +335,7 @@ class Boot(YamlBoot):
         生成 StatefulSet
         :params option 部署选项 {replicas, node}
                         replicas 副本数
-                        node 节点过滤，如 "beta.kubernetes.io/os": "linux" 或 "disk": "ssd"
+                        node 节点选择，如 "beta.kubernetes.io/os": "linux" 或 "disk": "ssd"
         '''
         option = self.fix_replicas_option(option, 'sts')
         yaml = {
@@ -275,7 +361,7 @@ class Boot(YamlBoot):
         生成部署
         :params option 部署选项 {replicas, node}
                         replicas 副本数
-                        node 节点过滤，如 "beta.kubernetes.io/os": "linux" 或 "disk": "ssd"
+                        node 节点选择，如 "beta.kubernetes.io/os": "linux" 或 "disk": "ssd"
         '''
         option = self.fix_replicas_option(option, 'deploy')
         yaml = {
@@ -287,7 +373,7 @@ class Boot(YamlBoot):
                 "selector": {
                     "matchLabels": self.build_labels()
                 },
-                "template": self.build_pod_template(option.get('node'), option.get('tolerations'))
+                "template": self.build_pod_template(option.get('nodes'), option.get('tolerations'))
             }
         }
         self.save_yaml(yaml, '-deploy.yml')
@@ -324,10 +410,10 @@ class Boot(YamlBoot):
             ret.append(item)
         return ret
 
-    def build_pod_template(self, node = None, tolerations = None):
+    def build_pod_template(self, nodes = None, tolerations = None):
         '''
         构建pod模板
-        :param node: 只有deploy才有node过滤器
+        :param nodes: 只有deploy才有node过滤器
         :param tolerations: 只有deploy才有容忍
         :return:
         '''
@@ -342,10 +428,10 @@ class Boot(YamlBoot):
             }
         }
         # 只有deploy才有node过滤器
-        if node:
-            if isinstance(node, str):
-                node = replace_var(node, False)
-            ret["spec"]["nodeSelector"] = node
+        if nodes:
+            if isinstance(nodes, str):
+                nodes = replace_var(nodes, False)
+            ret["spec"]["nodeSelector"] = nodes
         # 只有deploy才有容忍
         if tolerations:
             ret["spec"]["tolerations"] = self.build_tolerations(tolerations)
@@ -428,11 +514,8 @@ class Boot(YamlBoot):
             "ports": self.build_container_ports(get_and_del_dict_item(option, 'ports')),
             "resources": self.build_resources(get_and_del_dict_item(option, "resources")),
             "volumeMounts": self.build_volume_mounts(get_and_del_dict_item(option, "volumes")),
-            "readinessProbe": {
-                "exec": {
-                    "command": ["/usr/bin/check-status", "-r"]
-                }
-            }
+            "livenessProbe": self.build_probe(get_and_del_dict_item(option, "live?")),
+            "readinessProbe": self.build_probe(get_and_del_dict_item(option, "ready?")),
         }
         ret.update(option)
         del_dict_none_item(ret)
@@ -534,31 +617,59 @@ class Boot(YamlBoot):
             }
         # configmap: https://blog.csdn.net/weixin_45880055/article/details/117590045
         if protocol == 'config':
-            return {
+            ret = {
                 'configMap': {
                     'name': self._app + "-cfg",
-                    # 'items': [
-                    #     {'key': host_path, 'path': host_path}
-                    # ]
                 }
             }
+            # 指定items(配置挂载的key)
+            # items的作用是 1指定key挂载到不同名到文件上 2过滤要挂载的key，否则挂载全部key
+            # self._config_file_keys默认挂载的文件类型的key，仅当host_path没指定时用到
+            self.build_config_volume_items(host_path or self._config_file_keys, ret)
+            return ret
         # secret: https://www.cnblogs.com/litzhiai/p/11950273.html
         if protocol == 'secret':
-            return {
+            ret = {
                 'secret': {
                     'secretName': host_path,
                 }
             }
+            # 跟config实现一样
+            self.build_config_volume_items(host_path or self._secret_file_keys, ret)
+            return ret
         raise Exception(f'暂不支持卷协议: {protocol}')
+
+    def build_config_volume_items(self, keys, volume):
+        '''
+        指定items(配置挂载的key)
+        items的作用是 1指定key挂载到不同名到文件上 2过滤要挂载的key，否则挂载全部key
+        self._config_file_keys或self._secret_file_keys默认挂载的文件类型的key，仅当host_path没指定时用到
+        :param: host_path 对应要挂载的key
+        :param: config_file_keys 默认挂载的文件类型的key，仅当host_path没指定时用到
+        '''
+        # 1 没指定key，则挂载所有key，跳过指定items
+        if not keys:
+            return
+
+        # 2 有指定key
+        # 2.1 根据key拼接item
+        if isinstance(keys, str): # 单个key
+            keys = [keys]
+        items = [{'key': key, 'path': key} for key in keys]
+
+        # 2.2 记录到第二层的 items 属性
+        type = list(volume.keys())[0]
+        volume[type]['items'] = items
 
     def build_volume_mounts(self, mounts):
         '''
         构建目录映射
         :params mounts 多行，格式为
-                    dir:///apps/fpm729/etc/php-fpm/:/usr/local/etc/php-fpm.d/:rw
-                    file:///var/run/docker.sock:/var/run/docker.sock:ro
-                    nfs://192.168.159.14/data:/mnt
-                    config://mycfg:/etc/mycfg
+                    dir:///apps/fpm729/etc/php-fpm/:/usr/local/etc/php-fpm.d/:rw -- 挂载目录
+                    file:///var/run/docker.sock:/var/run/docker.sock:ro -- 挂载文件，只读
+                    nfs://192.168.159.14/data:/mnt -- 挂载nfs
+                    config://:/etc/nginx/conf.d -- 将configmap挂载为目录，如果有调用config_files()则挂载文件类型的key，否则挂载所有的key
+                    config://nginx.conf:/etc/nginx/nginx.conf -- 将configmap中key=nginx.conf的单个配置项挂载为文件，不同的key写不同的行
         '''
         if mounts is None or len(mounts) == 0:
             return None
@@ -567,22 +678,24 @@ class Boot(YamlBoot):
 
         ret = []
         for mount in mounts:
-            # 解析末尾的 rw ro
+            # 1 解析末尾的 rw ro
             ro = False # 是否只读
             mat = re.search(':r[wo]', mount)
             if mat is not None:
                 ro = mat.group(0) == ':ro'
             '''
-            解析协议
-            dir:///apps/fpm729/etc/php-fpm/:/usr/local/etc/php-fpm.d/:rw
-            file:///var/run/docker.sock:/var/run/docker.sock:ro
-            nfs://192.168.159.14/data:/mnt
-            config://mycfg:/etc/mycfg
+            2 解析协议
+            dir:///apps/fpm729/etc/php-fpm/:/usr/local/etc/php-fpm.d/:rw -- 挂载目录
+            file:///var/run/docker.sock:/var/run/docker.sock:ro -- 挂载文件，只读
+            /lnmp/www/:/www -- 挂载目录或文件
+            nfs://192.168.159.14/data:/mnt -- 挂载nfs
+            config://:/etc/nginx/conf.d -- 将configmap挂载为目录
+            config://nginx.conf:/etc/nginx/nginx.conf -- 将configmap中key=nginx.conf的单个配置项挂载为文件，不同的key写不同的行
             '''
             protocol = None
-            if "://" in mount:
+            if "://" in mount: # 有协议
                 #  mat = re.search('(\w+)://([\w\d\._]*)(/.+):(.+)', mount)
-                mat = re.search('(\w+)://(.+?):(.+)', mount)
+                mat = re.search('(\w+)://(.*?):(.+)', mount)
                 protocol = mat.group(1) # 协议
                 host_and_path = mat.group(2) # 主机 + 宿主机路径
                 mount_path = mat.group(3) # 容器中挂载路径
@@ -594,7 +707,7 @@ class Boot(YamlBoot):
                     host = ''
                     host_path = host_and_path
                 vol = self.build_volume(protocol, host, host_path)
-            elif ':' in mount: # /mycfg:/etc/mycfg
+            elif ':' in mount: # 无协议，有本地卷映射，如 /lnmp/www/:/www
                 host_path, mount_path = mount.split(':', 1)
                 vol = {
                     'hostPath': {
@@ -607,22 +720,129 @@ class Boot(YamlBoot):
                     "emptyDir": {}
                 }
 
-            # 记录挂载
+            # 3 记录挂载
             # name = 'vol-' + random_str(10)
             name = 'vol-' + md5(mount_path)
             yaml = {
                 "name": name,
                 "mountPath": mount_path
             }
-            if protocol == 'config':
+            # 一般configmap/secret要挂载为目录，但如果指定了 host_path 表示只挂载单个key为文件
+            # host_path为key，mountPath为挂载的容器文件路径
+            if (protocol == 'config' or protocol == 'secret') and host_path:
                 yaml['subPath'] = host_path
+            # 只读
             if ro:
                 yaml['readOnly'] = True
             ret.append(yaml)
 
-            # 记录卷
+            # 4 记录卷
             vol["name"] = name
             self._volumes.append(vol)
+        return ret
+
+    def build_probe(self, option):
+        '''
+        构建probe： livenessProbe/readinessProbe
+        probe文档：https://blog.csdn.net/Jerry00713/article/details/123894868
+        probe的go实现： https://vimsky.com/examples/detail/golang-ex-k8s.io.kubernetes.pkg.api-Probe-HTTPGet-method.html
+        '''
+        if not option:
+            return None
+
+        # 处理各种秒数参数，支持简写 i:initialDelaySeconds, p:periodSeconds, t:timeoutSeconds, s:successThreshold, f:failureThreshold，
+        seconds = self.build_probe_seconds(option['seconds'])
+        # 处理action参数
+        action = self.build_probe_action(option['action'])
+        # 合并参数
+        return dict(seconds, **action)
+
+    # probe的各种秒数参数名的简写映射
+    probe_second_field_short_map = {
+        'i': 'initialDelaySeconds',
+        'p': 'periodSeconds',
+        't': 'timeoutSeconds',
+        's': 'successThreshold',
+        'f': 'failureThreshold'
+    }
+
+    def build_probe_seconds(self, seconds):
+        '''
+        处理probe的各种秒数参数，支持简写，简写规则是用全写的首字母，具体映射如下 i:initialDelaySeconds, p:periodSeconds, t:timeoutSeconds, s:successThreshold, f:failureThreshold
+        1 支持dict类型参数
+        1.1 全写 {initialDelaySeconds: 5, periodSeconds: 5, timeoutSeconds: 5, successThreshold: 1, failureThreshold: 5}
+        1.2 简写 {i: 5, p: 5, t: 5, s: 1, f: 5}
+        2 支持str类型参数
+        2.1 全写 initialDelaySeconds=5 periodSeconds=5 timeoutSeconds=5 successThreshold=1 failureThreshold=5
+        2.2 简写 i=5 p=5 t=5 s=1 f=5
+        '''
+        # str转dict
+        if isinstance(seconds, str):
+            seconds = seconds.strip().replace(' ', '&') # 转为url的query string格式
+            seconds = dict(parse.parse_qsl(seconds)) # 解析为dict
+        # 处理dict中的简写，简写=用全写的首字母，要恢复全写
+        ret = {}
+        for k, v in seconds.items():
+            if k in self.probe_second_field_short_map:
+                k = self.probe_second_field_short_map[k]
+            ret[k] = int(v)
+        return ret
+
+    def build_probe_action(self, action):
+        ''''
+        构建probe的action
+        :param: action 动作
+                无协议：执行命令，如
+                http协议：
+                tcp协议
+        '''
+        # 1 无协议: 执行命令
+        if "://" not in action:
+            return {
+                "exec": {
+                    "command": re.split('\s+', action) # 空格分割
+                }
+            }
+
+        # 2 有协议：http或tcp
+        # 解析headers：有 -h a=1&b=2
+        headers = None
+        if '-h' in action:
+            action, headers = re.split('\s+-h\s+', action)
+        # 解析url
+        url = parse.urlparse(action)
+        host_and_port = url.netloc
+        if ':' in host_and_port:
+            host, port = host_and_port.split(':')
+            port = int(port)
+        else:
+            host = host_and_port
+            port = 80
+        # 2.1 tcp
+        if url.scheme == 'tcp':
+            return {
+                'tcpSocket': {
+                    'port': port
+                }
+            }
+        # 2.2 http/https
+        path = url.path or '/'
+        if url.query:
+            path = path + '?' + url.query
+        ret= {
+            'httpGet': {
+                'port': port,
+                'path': path,
+            }
+        }
+        # 有host
+        if host != 'localhost' and host != '127.0.0.1':
+            ret['httpGet']['host'] = host
+        # 有请求头
+        if headers:
+            headers = parse.parse_qsl(headers)
+            headers = [{'name': k, 'value': v[0]} for k, v in headers]
+            ret['httpGet']['httpHeaders'] = headers
         return ret
 
     # 用在变量赋值中的函数
@@ -657,6 +877,7 @@ class Boot(YamlBoot):
             }
         }
 
+    # 构建容器中的环境变量
     def build_env(self, env):
         if env is None or len(env) == 0:
             return None
